4a114a55c9c93174506b2b1326edd9b335147fa3
Daniel Leutwyler <daniel.leutwyler@gmx.ch>
2023-02-03 22:21:38 +0100

    Skeleton to resize file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,15 @@
 import express from 'express';
+import sharp from 'sharp';
+import { promises as fs } from 'fs';
+import path from 'path';
+
+
+
+
+//const fs = require('fs').promises;
 
 const routes = express.Router();
+// const fullFilePath = '../assets/full/Tanzboden.jpg'
 
 const returnHtml = `<!DOCTYPE html>
 <html>
@@ -13,11 +22,44 @@ const returnHtml = `<!DOCTYPE html>
   </body>
 </html>
 `
-
 routes.get('/images', (req, res) => {
   console.log(`filename: ${req.query.filename}`);
   console.log(`width: ${req.query.width}`);
   console.log(`heigth: ${req.query.width}`);
+  
+  const fullFilePath = path.join(__dirname, '../../assets/full/') + req.query.filename
+  const thumbFilePath = path.join(__dirname, '../../assets/thumb/') + req.query.filename
+
+  console.log(`path: ${fullFilePath}`)
+  // Check if requested file exists - report error if not
+
+  
+  fs.stat(fullFilePath).then(() => {
+    sharp(fullFilePath)
+    .resize({
+      width: 300,
+      height: 200,
+      fit: sharp.fit.cover,
+      position: sharp.strategy.entropy
+    })
+    .toFile(thumbFilePath)
+    .then(() => {
+      console.log('Resized image created');
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+).catch((error) => {
+  console.error(`File not found: ${error}`);
+});
+
+
+  
+
+  // 
+
+
  res.send(returnHtml);
 });
 
