8415beca1a43cdec6a425b3edf8e26856c7611a4
Daniel Leutwyler <daniel.leutwyler@gmx.ch>
2023-02-03 21:06:54 +0100

    Serving full image

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 
 app.use('/api', routes);
+app.use(express.static('assets/full'))
 
 app.listen(port, ()=> {
  console.log(`server started at localhost:${port}`)
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,23 @@ import express from 'express';
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
- res.send('Main api route');
+const returnHtml = `<!DOCTYPE html>
+<html>
+  <head>
+    <title>Example Page</title>
+  </head>
+  <body>
+    <h1>Hello Image</h1>
+    <img src="http://localhost:3000/Abfahrt.jpg" alt="Abfahrt">
+  </body>
+</html>
+`
+
+routes.get('/images', (req, res) => {
+  console.log(`filename: ${req.query.filename}`);
+  console.log(`width: ${req.query.width}`);
+  console.log(`heigth: ${req.query.width}`);
+ res.send(returnHtml);
 });
 
 export default routes;
