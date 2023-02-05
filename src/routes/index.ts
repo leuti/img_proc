b5265e10bb579cd6f81f08429d603d3babab95b1
Daniel Leutwyler <daniel.leutwyler@gmx.ch>
2023-02-05 07:49:19 +0100

    Base setup; dynamic URL not working

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,66 +1,58 @@
-import express from 'express';
+import express, { Router } from 'express';
 import sharp from 'sharp';
 import { promises as fs } from 'fs';
 import path from 'path';
 
+const routes: Router = express.Router();
+// const thumbUrl = 'http://localhost:3000/Abfahrt.jpg';
+let thumbUrl = '';
+const thumbAlt = '';
+let fullUrl = '';
 
-
-
-//const fs = require('fs').promises;
-
-const routes = express.Router();
-// const fullFilePath = '../assets/full/Tanzboden.jpg'
-
-const returnHtml = `<!DOCTYPE html>
-<html>
-  <head>
-    <title>Example Page</title>
-  </head>
-  <body>
-    <h1>Hello Image</h1>
-    <img src="http://localhost:3000/Abfahrt.jpg" alt="Abfahrt">
-  </body>
-</html>
-`
 routes.get('/images', (req, res) => {
   console.log(`filename: ${req.query.filename}`);
   console.log(`width: ${req.query.width}`);
   console.log(`heigth: ${req.query.width}`);
   
-  const fullFilePath = path.join(__dirname, '../../assets/full/') + req.query.filename
-  const thumbFilePath = path.join(__dirname, '../../assets/thumb/') + req.query.filename
+  fullUrl = path.join(__dirname, '../../assets/full/') + req.query.filename
+  thumbUrl = path.join(__dirname, '../../assets/thumb/') + req.query.filename
 
-  console.log(`path: ${fullFilePath}`)
+  console.log(`path: ${fullUrl}`)
+
   // Check if requested file exists - report error if not
+  fs.stat(fullUrl).then(() => {
 
+    sharp(fullUrl)
+      .resize({
+        width: 300,
+        height: 200,
+        fit: sharp.fit.cover,
+        position: sharp.strategy.entropy
+      })
+      .toFile(thumbUrl)
+      .then(() => {
+        console.log('Resized image created');
+        const returnHtml = `<!DOCTYPE html>
+          <html>
+            <head>
+              <title>Example Page</title>
+            </head>
+            <body>
+              <h1>Hello Image</h1>
+              <img src="${thumbUrl}" alt="${thumbAlt}">
+            </body>
+          </html>
+          `
+        res.send(returnHtml);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    }
+  ).catch((error) => {
+    console.error(`File not found: ${error}`);
+  });
   
-  fs.stat(fullFilePath).then(() => {
-    sharp(fullFilePath)
-    .resize({
-      width: 300,
-      height: 200,
-      fit: sharp.fit.cover,
-      position: sharp.strategy.entropy
-    })
-    .toFile(thumbFilePath)
-    .then(() => {
-      console.log('Resized image created');
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  }
-).catch((error) => {
-  console.error(`File not found: ${error}`);
-});
-
-
-  
-
-  // 
-
-
- res.send(returnHtml);
 });
 
 export default routes;
